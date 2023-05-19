cdbe4ab686da13c0dd41e4e6970c3f9def7c6bf7
Ganapathi Reddy <119723312+ganapathireddy12@users.noreply.github.com>
2023-05-19 13:44:56 +0530

    #include<stdio.h> int main() {     int n,v,r,rev=0,f=0,c=0;     scanf("%d",&n);     while(n!=0)     {         v=n%10;         f=f*10+v;         n=n/10;     }     while(f)     {         r=f%10;         if(f%10==6 && c==0)         {             rev=(rev*10)+9;             c++;         }         else         {             rev=(rev*10)+r;         }         f=f/10;     }     printf("%d",rev); }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n,r=0,rev=0,c=0,v,f;
+    int n,v,r,rev=0,f=0,c=0;
     scanf("%d",&n);
     while(n!=0)
     {
         v=n%10;
-        r=r*10+v;
+        f=f*10+v;
         n=n/10;
     }
-    while(r)
+    while(f)
     {
-        f=r%10;
-        if(r%10==6 && c==0)
+        r=f%10;
+        if(f%10==6 && c==0)
         {
             rev=(rev*10)+9;
             c++;
         }
         else
         {
-            rev=(rev*10)+f;
+            rev=(rev*10)+r;
         }
-        r=r/10;
+        f=f/10;
     }
     printf("%d",rev);
 }
