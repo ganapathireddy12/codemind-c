20cbe595bc64b031e442490ea6e190907c49d4fc
Ganapathi Reddy <119723312+ganapathireddy12@users.noreply.github.com>
2023-05-23 22:19:30 +0530

    #include <stdio.h>
    int factorial(int num) {
        int fact = 1;
        for (int i = 2; i <= num; i++) {
            fact *= i;
        }
        return fact;
    }
    
    int isStrong(int num) {
        int originalNum = num;
        int sum = 0;
        while (num > 0) {
            int digit = num % 10;
            sum += factorial(digit);
            num /= 10;
        }
        return (sum == originalNum);
    }
    
    int main() {
        int T;
        scanf("%d", &T);
        
        while (T--) {
            int N;
            scanf("%d", &N);
            
            if (isStrong(N)) {
                printf("Strong
    ");
            } else {
                printf("Not Strong
    ");
            }
        }
        
        return 0;
    }

--- Strong_Number.c
+++ Strong_Number.c
@@ -1,24 +1,40 @@
-#include<stdio.h>
-int main()
-{
-   int n,i;
-   int fact,rem;
-   scanf("%d",&n);
-   int sum = 0;
-   int temp = n;
-   while(n){
-      i = 1,fact = 1;
-      rem = n % 10;
-      while(i <= rem){
-         fact = fact * i;
-         i++;
-      }
-      sum = sum + fact;
-      n = n / 10;
-   }
-   if(sum == temp)
-      printf("The number %d is a strong number",temp);
-   else
-      printf("The number %d is not a strong number",temp);
+#include <stdio.h>
 
+int factorial(int num) {
+    int fact = 1;
+    for (int i = 2; i <= num; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int isStrong(int num) {
+    int originalNum = num;
+    int sum = 0;
+    while (num > 0) {
+        int digit = num % 10;
+        sum += factorial(digit);
+        num /= 10;
+    }
+    return (sum == originalNum);
+}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+        
+        if (isStrong(N)) {
+            printf("Strong
+");
+        } else {
+            printf("Not Strong
+");
+        }
+    }
+    
+    return 0;
 }
