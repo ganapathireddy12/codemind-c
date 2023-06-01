51f06d2a631962865bda60ce451849269120a14d
Ganapathi Reddy <119723312+ganapathireddy12@users.noreply.github.com>
2023-06-01 12:32:26 +0530

    #include<stdio.h>
    int main()
    {
        int n,i,j;
        scanf("%d",&n);
        for(i=1;i<=n;i++)
        {
            for(j=1;j<=(n-i);j++)
            {
                printf(" ");
            }
            for(;j<n+i;j++)
            {
                printf("%d",i);
            }
            
            printf("
    ");
        }
        
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -2,20 +2,21 @@
 
 int main()
 {
-    int n,i,j,k;
+    int n,i,j;
     scanf("%d",&n);
-    for( i=1;i<=n;i++)
+    for(i=1;i<=n;i++)
     {
-        for( j=1;j<=n-2;j++)
+        for(j=1;j<=(n-i);j++)
         {
-            printf("%d",j);
+            printf(" ");
         }
-        k=n-3;
-        for( j=k;j>=1;j--)
+        for(;j<n+i;j++)
         {
-            printf("%d",j);
+            printf("%d",i);
         }
+        
         printf("
 ");
     }
+    
 }
